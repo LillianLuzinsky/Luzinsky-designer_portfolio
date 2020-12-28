4f73edd7c34bff1871a3394a95a531a296888df4
Lillian Luzinsky <Eye.RisConcepts@192-168-1-100.tpgi.com.au>
2020-12-28 17:53:55 +1100

    updating mobile navbar

--- js/script.js
+++ js/script.js
@@ -11,6 +11,21 @@
     let scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
   }
+
+  //Get att the tabs
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach(clickedTab => {
+    //Add onClick event
+    clickedTab.addEventListener('click', () => {
+        //Remove the active class from all tabs
+        tabs.forEach(tab => {
+            tab.classList.remove("active");
+        })
+        //Add the active class on the clicked tab
+        clickedTab.classList.add('active');
+    })
+})
   
   console.log(
     "%cThank you for taking the time to look at my portfolio, I hope you enjoyed browsing through. Feel free to contact me, I would love to hear from your feedback. Have a good day :)",
